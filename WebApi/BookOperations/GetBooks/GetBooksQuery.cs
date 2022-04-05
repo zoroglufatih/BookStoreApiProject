f909939d7394a34aa7537246f62d05e0cbb6f025
fatihzoroglu <fatihzorogluu@gmail.com>
2022-04-05 17:10:36 +0300

    ok

---
+++ WebApi/BookOperations/GetBooks/GetBooksQuery.cs
@@ -0,0 +1,43 @@
+using System.Collections.Generic;
+using System.Linq;
+using WebApi.Common;
+using WebApi.DBOperations;
+
+namespace WebApi.BookOperations.GetBooks
+{
+    public class GetBooksQuery
+    {
+        private readonly BookStoreDbContext _dbContext;
+        public GetBooksQuery(BookStoreDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public List<BooksViewModel> Handle()
+        {
+            var bookList = _dbContext.Books.OrderBy(x=> x.Id).ToList<Book>();
+
+            List<BooksViewModel> vm = new List<BooksViewModel>();
+            foreach(var item in bookList)
+            {
+                vm.Add(new BooksViewModel(){
+                    Title = item.Title,
+                    Genre = ((GenreEnum)item.GenreId).ToString(),
+                    PublishDate = item.PublishDate.Date.ToString("dd/MM/yyy"),
+                    PageCount = item.PageCount
+                });
+            }
+
+            return vm;
+        }
+    }
+
+    public class BooksViewModel
+    {
+        public string Title { get; set; }
+        public int PageCount { get; set; }
+        public string PublishDate { get; set; }
+        public string Genre { get; set; }
+
+    }
+}
---
+++ WebApi/BookOperations/UpdateBook/UpdateBookCommand.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using WebApi.Common;
+using WebApi.DBOperations;
+
+namespace WebApi.BookOperations.UpdateBook
+{
+    public class UpdateBookCommand
+    {
+        private readonly BookStoreDbContext _dbContext;
+        public int BookId { get; set; }
+        public UpdateBookModel Model { get; set; }
+        public UpdateBookCommand(BookStoreDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public void Handle()
+        {
+            var book = _dbContext.Books.SingleOrDefault(x=> x.Id == BookId);
+
+            if(book == null)
+            {
+                throw new InvalidOperationException("Güncellenecek Kitap Bulunamadı!");
+            }
+
+            book.GenreId = Model.GenreId != default ? Model.GenreId : book.GenreId;
+            book.Title = Model.Title != default ? Model.Title : book.Title;
+
+            _dbContext.SaveChanges();
+        }
+    }
+
+    public class UpdateBookModel
+    {
+        public string Title { get; set; }
+        public int GenreId { get; set; }
+    }
+}
